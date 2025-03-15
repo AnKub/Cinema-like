c603324260fc17a0d7a960aa0549027339fe0f56
AnKub <ankub3088@gmail.com>
2025-03-15 17:40:14 +0200

    edp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Search from './components/Search'
 
-const API_BASE_URL = 'http://api.themoviedb.org/3';
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS ={
 method: 'GET',
 headers: {
   accept: 'application/json',
- Authorizaton: `Beatet ${API_KEY}`
+ Authorizaton: `Bearer ${API_KEY}`
 }
 }
 
@@ -15,18 +15,36 @@ headers: {
 
           const [searchTerm, setSearchTerm] = useState('');
           const [errorMessage, setErrorMessage] = useState('');
+          const [movieList, setMovieList] = useState ([]);
+          const [isLoading, setIsloading] = useState (false);
 
         const fetchMovies = async () => {
+          
+          setIsloading(true);
+          setErrorMessage('');
+
           try{
             const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
             const response = await fetch(endpoint, API_OPTIONS);
-            alert (response)
+
+            if(!response.ok){
+              throw new Error('Failed to fetch movies')
+            }
+            const data = await response.json()
+            if(data.Response === 'False'){
+              setErrorMessage(data.Error || 'Failed to fetch movies');
+              setMovieList([]);
+              return;
+            }
+            setMovieList(data.results || [])
+
           } catch (error){
-            console.log(`Error fetching movie error: ${error}`);
-            setErrorMessage('Error fetching movies, Please try again later.');
+            console.log(`Error fetching movies: ${error}`);
+            setErrorMessage('Error fetching movies. Please try again later.');
+          } finally {
+            setIsloading(false);
           }
         }
-
 useEffect(()=>{
 fetchMovies()
 }, [])
@@ -41,10 +59,19 @@ fetchMovies()
          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
        </header>
 
-      <section className='all=movies'>
-        <h2>All movie</h2>
-
-        {errorMessage && <p className='error-message'>{errorMessage}</p>};
+      <section className='all-movies'>
+        <h2>All movies</h2>
+       {isLoading ? (
+        <p className='text-white'>Loading...</p>
+       ) : errorMessage ? (
+        <p className='text-orange-500'>{errorMessage}</p>
+       ) : (
+        <ul>
+          {movieList.map((movie) => (
+            <p className='text-white'>{movie.title}</p>
+          ))}
+        </ul>
+       )}
       </section>
       </div>
 
